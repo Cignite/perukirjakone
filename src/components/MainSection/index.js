ea85103b5335cb7aafb2ec3937ba59873d170386
Thaman <thaman.chand@invian.fi>
2020-06-01 20:33:23 +0545

    MainSection compponent

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -1,40 +1,43 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
+import Img from '../images/couple_big.jpg';
+import logo from '../images/logo-white.svg'
 import "./styles.scss";
 
 function MainSection(props) {
   return (
-    <section className="section">
-      <div className="container">
-        <div className="columns is-vcentered is-desktop">
-          <div className="column is-5-desktop has-text-centered-touch">
-            <header className="MainSection__header">
-              <h1 className="title is-spaced is-1 has-text-weight-bold">
-                Perukone
-              </h1>
-              <p className="MainSection__subtitle subtitle">
-                This is call to action message
-              </p>
-            </header>
+    <div class="hero is-fullheight is-primary has-background">
+      <img alt="Fill Murray" class="hero-background" src={Img} />
+        <div class="hero-head">
+          <nav class="navbar">
+            <div class="container">
+              <div class="navbar-brand">
+                <Link to="/" class="navbar-item">
+                  <img src={logo} alt="Perukirjakone" />
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            We digitalise your last will
+          </h1>
+          <h3 class="subtitle">
+            More description here
+          </h3>
+          <Link to="/register">
             <button
               className="button is-medium is-primary"
-              onClick={() => {
-                const el = document.getElementById("pricing");
-                el.scrollIntoView({ behavior: "smooth", block: "start" });
-              }}
             >
-              Buy now €29,99
+              Buy €29,99
             </button>
-          </div>
-          <div className="column is-1" />
-          <div className="column">
-            <figure className="MainSection__image image">
-              <img src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/japan_ubgk.svg" />
-            </figure>
-          </div>
+          </Link>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
