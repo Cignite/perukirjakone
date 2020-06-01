b527f503a5bcf310e1614fe79c4ae5484e86c362
Thaman <thaman.chand@invian.fi>
2020-06-01 20:34:04 +0545

    NotFound page component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,17 @@
+/**
+ * NotFoundPage
+ *
+ * This is the page we show when the user visits a url that doesn't have a route
+ */
+
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <article>
+      <h1>
+        Oh! page you're can't be found!
+      </h1>
+    </article>
+  );
+}
