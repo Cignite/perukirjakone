17d8797c70dbc7f05a125886eb34b1cdd9e91567
Thaman <thaman.chand@invian.fi>
2020-06-04 15:58:57 +0545

    Datepicker shared component

---
+++ src/components/Shared/Datepicker/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import './styles.scss';
+
+const DatePickerWrapper = props => {
+  const {
+    input: { value },
+    label,
+    minDate,
+    maxDate,
+    meta,
+  } = props;
+  const [startDate, setStartDate] = useState();
+
+  return (
+    <div className="field">
+      <label className="label">{label}</label>
+      <div className="control">
+        <DatePicker
+          className={meta.touched && meta.error ? "form__form-group-datepicker__error" : "form__form-group-datepicker"}
+          dateFormat="dd.MM.yyyy"
+          showPopperArrow={false}
+          showMonthDropdown
+          dropdownMode="select"
+          selected={startDate}
+          onChange={value => {
+            setStartDate(value);
+            props.input.onChange(value);
+          }}
+          minDate={new Date()}
+          todayButton="Today"
+          onBlur={props.input.onBlur}
+          onFocus={props.input.onFocus}
+          value={props.input.value}
+          placeholderText="01.02.2020"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DatePickerWrapper;
