d95e8303aae103adbf7d014d8e73da937284a7e5
Thaman <thaman.chand@invian.fi>
2020-06-04 16:00:00 +0545

    Input shared component

---
+++ src/components/WizardForm/components/InputAdapter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Input = ({ input, meta, label, placeholder }) => {
+  console.log("meta", meta)
+  return (
+    <div className="field">
+      <label className="label">{label}</label>
+      <div className="control">
+        <input
+          placeholder={placeholder}
+          {...input}
+          className={meta.touched && meta.error ? "input is-danger" : "input"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
