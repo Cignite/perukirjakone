54be6df3f6a4a98279e8eba00ee8fb92dfa6b1c6
Thaman <thaman.chand@invian.fi>
2020-06-10 14:49:25 +0545

    set user-validation, user-verification, form-wizard Route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,10 +8,15 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from '../Homepage';
 import RegisterPage from '../Registerpage';
+//import WizardForm from '../StepWizard';
+// import EmailRegistration from '../StepWizard/components/EmailRegistration';
+import UserRegistration from '../UserRegistration';
+import UserVerification from '../Verification';
+
 import NotFoundPage from '../NotFound';
 
 import MainWrapper from '../Wrapper';
@@ -23,7 +28,12 @@ export default function App() {
         <main>
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/user-validation" component={UserRegistration} />
+            <Route exact path="/user-verification" component={UserVerification} />
+
+            <Route exact path="/wizard-form" component={RegisterPage} />
+            {/*<Route path="/create" component={WizardForm} />
+          <Redirect from="/" to="/create/step1" />*/}
             <Route component={NotFoundPage} />
           </Switch>
         </main>
