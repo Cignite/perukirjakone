e7f512ae96f14e4ee8098a82ad7c633945afed92
Thaman <thaman.chand@invian.fi>
2020-06-10 14:48:25 +0545

    fix className, onClick Handler

--- src/components/UserRegistration/index.js
+++ src/components/UserRegistration/index.js
@@ -1,60 +1,103 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+import { Field, Form } from "react-final-form";
+import * as EmailValidator from "email-validator";
+
+import InputWrapper from '../Shared/Input';
+import Error from '../Shared/Error';
 import logo from '../images/logo.svg'
 
+import './styles.scss';
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const onSubmit = async values => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
+
+
 const UserRegistration = () => {
+
+  const submitEmail = (payload) => {
+    console.log("submitEmail", payload);
+  }
   return(
     <section className="hero is-fullheight is-primary is-bold">
       <div className="hero-body">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-4-tablet is-4-desktop is-4-widescreen">
-              <form action="" className="box ">
+              <div action="" className="box ">
                 <div className="section has-text-centered">
                   <Link to="/" className="logo">
-                    <img src={logo} alt="Perukirjakone" classNameName="has-text-centered" />
+                    <img src={logo} alt="Perukirjakone" className="has-text-centered" />
                   </Link>
                 </div>
                 <div className="field has-text-centered">
                   <label className="label is-size-5">Email validation </label>
+                    <p className="is-small">We need email to verify right person is using
+                      Perukirjakone service.</p>
                 </div>
                 <br/>
-                <div className="field">
 
-                  <div className="control has-icons-left">
-                    <input type="text" placeholder="Email" className="input" required />
-                    <span className="icon is-small is-left">
-                      <i className="fa fa-user"></i>
-                    </span>
-                  </div>
-                </div>
+                  <Form
+                    validate={values => { // validate both passowrds are same
+                      const errors = {};
+                      if (!values.email) {
+                        errors.email = 'Email can not be empty';
+                      } if (!EmailValidator.validate(values.email)) {
+                        errors.email = "Invalid email address.";
+                      }
+                      return errors
+                    }}
+                    onSubmit={onSubmit}
+                    render={({ handleSubmit, pristine, values, submitting, invalid}) => (
+                      <form onSubmit={handleSubmit} className="user-validation">
+                        <div className="columns">
+                          <div className="column">
+                            <Field
+                              name="email"
+                              component={InputWrapper}
+                              type="email"
+                              placeholder="hello@abc.fi"
+                              label="Email"
+                            />
+                            <Error name="email" />
+                          </div>
+                        </div>
+                        <div className="field">
+                          <label className="checkbox">
+                            <input type="checkbox" />
+                            &nbsp;I agree to the terms and services and privacy policy
+                          </label>
+                        </div>
 
-                <div className="field">
-                  <label className="checkbox">
-                    <input type="checkbox" />
-                    &nbsp;I agree to the terms and services and privacy policy
-                  </label>
-                </div>
+
+                        <div className="field level-right">
+                          <button className="button is-success is-fullwidth" onClick={() => submitEmail(values)}>
+                            Validate email
+                          </button>
+                        </div>
+                      </form>
+                    )}
+                  />
 
 
-                <div className="field level-right">
-                  <a className="button is-success is-fullwidth">
-                    Validate email
-                  </a>
-                </div>
                 <br/>
                 <p className="has-text-centered">
-                <a className="label has-text-weight-light">
+                <a className="label has-text-weight-bold">
                     Already validated email?
                   </a>
                 </p>
-              <p className="has-text-centered">
-                <Link to="/user-verification" className="label has-text-weight-light">
-                  Code verification
+
+                <h4 className="has-text-centered">
+                  <Link to="/user-verification" className="has-text-weight-light">
+                    Code verification
                 </Link>
-              </p>
-          </form>
+              </h4>
+          </div>
         </div>
       </div>
     </div>
