c473a14b79b3da38b0f05d023a01f755eb6022ba
Thaman <thaman.chand@invian.fi>
2020-06-13 18:38:20 +0545

    add disabled props

--- src/components/Shared/Input/index.js
+++ src/components/Shared/Input/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Input = ({ input, meta, label, placeholder }) => {
-  console.log("meta", meta)
+const Input = ({ input, meta, label, placeholder, disabled }) => {
   return (
     <div className="field">
       <label className="label">{label}</label>
       <div className="control">
         <input
+          disabled={disabled}
           placeholder={placeholder}
           {...input}
           className={meta.touched && meta.error ? "input is-danger" : "input"}
