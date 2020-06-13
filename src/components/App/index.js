4fde481b4bc6e8b1c62000faf2396fb4306e1b48
Thaman <thaman.chand@invian.fi>
2020-06-13 18:37:29 +0545

    Clean up

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,14 +11,10 @@ import PropTypes from 'prop-types';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import HomePage from '../Homepage';
-import RegisterPage from '../Registerpage';
-//import WizardForm from '../StepWizard';
-// import EmailRegistration from '../StepWizard/components/EmailRegistration';
+import PerukirjaForm from '../PerukirjaForm';
 import UserRegistration from '../UserRegistration';
 import UserVerification from '../Verification';
-
 import NotFoundPage from '../NotFound';
-
 import MainWrapper from '../Wrapper';
 
 export default function App() {
@@ -30,10 +26,7 @@ export default function App() {
             <Route exact path="/" component={HomePage} />
             <Route exact path="/user-validation" component={UserRegistration} />
             <Route exact path="/user-verification" component={UserVerification} />
-
-            <Route exact path="/wizard-form" component={RegisterPage} />
-            {/*<Route path="/create" component={WizardForm} />
-          <Redirect from="/" to="/create/step1" />*/}
+            <Route exact path="/wizard-form" component={PerukirjaForm} />
             <Route component={NotFoundPage} />
           </Switch>
         </main>
