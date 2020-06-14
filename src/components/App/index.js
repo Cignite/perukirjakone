e14255208c1373f972b02fde61701d4cbad1936f
Thaman <thaman.chand@invian.fi>
2020-06-14 12:03:23 +0545

    remove redirect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../Homepage';
 import PerukirjaForm from '../PerukirjaForm';
