912b7dad31e6a1b6a191046f6e25c9cef297c206
Thaman <thaman.chand@invian.fi>
2020-06-14 20:04:55 +0545

    generate initialValues to test

---
+++ src/components/WizardForm/initialValues.js
@@ -0,0 +1,152 @@
+const initialValues = {
+  "deceasedPerson": [
+    {
+      "relationType": "mies"
+    }
+  ],
+  "propertyInfo": [
+    {
+      "name": "Talo",
+      "value": "50m"
+    },
+    {
+      "name": "House",
+      "value": "50k"
+    }
+  ],
+  "bankAccountInfo": [
+    {
+      "number": "FI40",
+      "value": "50"
+    },
+    {
+      "number": "FI50",
+      "value": "549"
+    }
+  ],
+  "shareInfo": [
+    {
+      "value": "50 ",
+      "number": "NOIKIA STOCK"
+    },
+    {
+      "name": "",
+      "value": "50",
+      "number": "TELE SONERA"
+    }
+  ],
+  "debtInfo": [
+    {
+      "name": "Car insuranc",
+      "value": "40"
+    }
+  ],
+  "propertyLikeSofaWatchInfo": [
+    {
+      "value": "50",
+      "name": "Rolex"
+    },
+    {
+      "name": "Painting",
+      "value": "50"
+    }
+  ],
+  "personalBelongingsInfo": [
+    {
+      "name": "sculpture",
+      "value": "50"
+    },
+    {
+      "name": "hello kitty",
+      "value": "50"
+    }
+  ],
+  "widowStockInfo": [
+    {
+      "name": "",
+      "amount": ""
+    }
+  ],
+  "FESukuselvitykset": [
+    {
+      "sukuselvitykset": ""
+    }
+  ],
+  "otherExpenses": [
+    {
+      "info": ""
+    }
+  ],
+  "whoWasPresent": [
+    {
+      "name": "Person present",
+      "city": "Turku"
+    }
+  ],
+  "widowPersonalBelonings": [
+    {
+      "name": "",
+      "value": ""
+    }
+  ],
+  "widowProperty": [
+    {
+      "name": "",
+      "value": ""
+    }
+  ],
+  "widowBankInfo": [
+    {
+      "name": "",
+      "value": ""
+    }
+  ],
+  "widowBankAccountInfo": [
+    {
+      "value": "50M",
+      "number": "NASDAQ"
+    }
+  ],
+  "isThereLifeInsurance": true,
+  "insuranceName": "If surance",
+  "insuranceAmount": "49k",
+  "insuranceDate": "2020-06-29T18:15:00.000Z",
+  "isOtherAnnouncement": true,
+  "doesWidowWantsToLiveInSameApartment": true,
+  "haveAllPartiesInvited": true,
+  "customerFirstName": "Thaman",
+  "customerlastName": "Chand",
+  "customerSSN": "03030",
+  "address": "Kuusitie5",
+  "customerTimeOfDeath": "2020-06-23T18:15:00.000Z",
+  "agreeementPlace": "Turku",
+  "shareholderName": "Pratima",
+  "shareholderSSN": "Sen",
+  "isTestamentDeceasedPropertyAssignedChecked": true,
+  "testamentPropertyAssignInfo": "Hannu Harala",
+  "isSukuselvitykset": true,
+  "isTestamenttiChecked": true,
+  "testamenttiTimeOfDeath": "2020-07-01T18:15:00.000Z",
+  "isMarriedSettlementContractChecked": true,
+  "marriageSettlementDate": "2020-07-02T18:15:00.000Z",
+  "isOtherDocumentChecked": true,
+  "otherDocumentInfo": "here are other documents",
+  "didDeceasedHadCarBoat": true,
+  "deceasedCarBrandTypeInfo": "Audi",
+  "deceasedCarBrandTypeValue": "30k",
+  "deceasedMotorBikeBrandTypeInfo": "motorbike",
+  "deceasedMotorBikeBrandTypeValue": "50k",
+  "isPropertyLikeSofa": true,
+  "isHKIrtaminen": true,
+  "didDeceasedHaveDebt": true,
+  "inviteeName": "Hello",
+  "inviteePlace": "Tampere",
+  "inviteeDate": "2020-06-23T18:15:00.000Z",
+  "trustedMenNameFirst": "Person1",
+  "trustedMenPlaceFirst": "Turku",
+  "trustedMenNameSecond": "Person2",
+  "trustedMenPlaceSecond": "helsinki"
+}
+
+
+export default initialValues;
