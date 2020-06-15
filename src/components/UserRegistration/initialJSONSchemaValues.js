7fb4e676460145ef68de49482a6404b9b9c31f0a
Thaman <thaman.chand@invian.fi>
2020-06-15 15:23:25 +0545

    initialValues payload to post

---
+++ src/components/UserRegistration/initialJSONSchemaValues.js
@@ -0,0 +1,89 @@
+const initialJSONSchemaValues = {
+  deceasedPerson: [
+    { relationType: "" },
+  ],
+  propertyInfo: [
+    {
+      name: "",
+      value: ""
+    },
+  ],
+  bankAccountInfo: [
+    {
+      number: "" ,
+      value: ""
+    },
+  ],
+  shareInfo: [
+    {
+      number: "",
+      value: ""
+    },
+  ],
+  debtInfo: [
+    {
+      name: "",
+      value: ""
+    },
+  ],
+  propertyLikeSofaWatchInfo: [
+    {
+      name: "",
+      value: ""
+    }
+  ],
+  personalBelongingsInfo: [
+    {
+      name: "",
+      value: ""
+    }
+  ],
+  widowStockInfo: [
+    {
+      name: "",
+      value: "",
+    }
+  ],
+  funeralExpensesInfo: [
+    {
+      familyReport: "",
+    }
+  ],
+  otherFuneralExpenses: [
+    {
+      info: ""
+    }
+  ],
+  whoWasPresent: [
+    {
+      name: "",
+      city: ""
+    }
+  ],
+  widowPersonalBelonings: [
+    {
+        name: "",
+        value: "",
+    }
+  ],
+  widowProperty: [
+    {
+      name: "",
+      value:""
+    }
+  ],
+  widowBankInfo: [
+    {
+      name: "",
+      value: "",
+    }
+  ],
+  widowBankAccountInfo: [
+    {
+      number: "",
+      value: ""
+    }
+  ]
+};
+
+export default initialJSONSchemaValues;
