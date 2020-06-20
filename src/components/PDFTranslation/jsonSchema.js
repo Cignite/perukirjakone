cec9ba462b98b38734856bfa8f49b4adfc465252
Thaman <thaman.chand@invian.fi>
2020-06-20 12:39:08 +0545

    populate jsonSchema to translate into pdf document

---
+++ src/components/PDFTranslation/jsonSchema.js
@@ -0,0 +1,174 @@
+const jsonSchema = {
+  "deceasedPerson": [
+    {
+      "relationType": "Poika"
+    }
+  ],
+  "propertyInfo": [
+    {
+      "name": "Helsinki asunto",
+      "value": "400k"
+    },
+    {
+      "name": "Turku mokki",
+      "value": "50k"
+    }
+  ],
+  "bankAccountInfo": [
+    {
+      "number": "FI30",
+      "value": "40"
+    },
+    {
+      "number": "FI50",
+      "value": "50"
+    }
+  ],
+  "shareInfo": [
+    {
+      "value": "50k",
+      "number": "Nordnet"
+    }
+  ],
+  "debtInfo": [
+    {
+      "name": "this is debt info",
+      "value": "40"
+    },
+    {
+      "name": "this is debit info 2",
+      "value": "5000"
+    }
+  ],
+  "propertyLikeSofaWatchInfo": [
+    {
+      "name": "WATCH",
+      "value": "40"
+    }
+  ],
+  "personalBelongingsInfo": [
+    {
+      "value": "40K",
+      "name": "PAINTING"
+    }
+  ],
+  "widowStockInfo": [
+    {
+      "name": "Teli sonera",
+      "value": "40 "
+    }
+  ],
+  "funeralExpensesInfo": [
+    {
+      "familyReport": "4000"
+    },
+    {
+      "familyReport": "6600"
+    }
+  ],
+  "otherFuneralExpenses": [
+    {
+      "info": "6000"
+    },
+    {
+      "info": "9000"
+    },
+
+  ],
+  "whoWasPresent": [
+    {
+      "name": "Tomi Hankala",
+      "city": "Tampere"
+    },
+    {
+      "name": "Hanko Nimi",
+      "city": "Porvoo"
+    }
+  ],
+  "widowPersonalBelonings": [
+    {
+      "name": "ROLEX",
+      "value": "50"
+    },
+    {
+      "name": "GOLD",
+      "value": "59"
+    }
+  ],
+  "widowProperty": [
+    {
+      "name": "WIDOW PROPERTY",
+      "value": "50"
+    }
+  ],
+  "widowBankInfo": [
+    {
+      "value": "400K",
+      "name": "FI50"
+    }
+  ],
+  "widowBankAccountInfo": [
+    {
+      "number": "mero stock",
+      "value": "40"
+    }
+  ],
+  "personalWorthInfo": [
+    {
+      "name": "Rolex",
+      "value": "500"
+    }
+  ],
+  "customerFirstName": "Ram",
+  "customerlastName": "Bahdur",
+  "customerSSN": "303030",
+  "address": "Hammenkatu 1B",
+  "customerTimeOfDeath": "2020-06-23T18:15:00.000Z",
+  "agreeementPlace": "Hammenkatu4",
+  "shareholderName": "Hannu harala",
+  "shareholderSSN": "5050",
+  "funeralExpensesInfoDeathCertificate": "9000",
+  "flowers": "5000",
+  "funeralExpensesInfoTombstone": "70000",
+  "perukirjakoneReward": "400",
+  "wasDeceasedPersonMarried": true,
+  "isPersonalWorth": true,
+  "isPersonalWorthInfo": "watch ",
+  "isPersonalWorthValue": "40",
+  "didDeceasedHaveDebt": true,
+  "verifyInfoProvidedIsCorrect": true,
+  "haveAllPartiesInvited": true,
+  "doesWidowWantsToLiveInSameApartment": true,
+  "isThereLifeInsurance": true,
+  "isOtherAnnouncement": true,
+  "insuranceName": "If surance",
+  "insuranceAmount": "20k",
+  "insuranceDate": "2020-06-30T18:15:00.000Z",
+  "isSukuselvitykset": true,
+  "isTestamentDeceasedPropertyAssignedChecked": true,
+  "testamentPropertyAssignInfo": "Hannu harala",
+  "isTestamenttiChecked": true,
+  "testamenttiTimeOfDeath": "2020-07-01T18:15:00.000Z",
+  "isMarriedSettlementContractChecked": true,
+  "marriageSettlementDate": "2020-06-23T18:15:00.000Z",
+  "isOtherDocumentChecked": true,
+  "otherDocumentInfo": "other doc",
+  "didDeceasedHadCarBoat": true,
+  "deceasedCarBrandTypeInfo": "Audi A4",
+  "deceasedCarBrandTypeValue": "20k",
+  "deceasedMotorBikeBrandTypeInfo": "BMW",
+  "deceasedMotorBikeBrandTypeValue": "20K",
+  "isPropertyLikeSofa": true,
+  "isHKIrtaminen": true,
+  "isPersonalBelonings": true,
+  "inviteeName": "Invitee 1",
+  "inviteePlace": "Invitee Place",
+  "inviteeDate": "2020-06-23T18:15:00.000Z",
+  "trustedMenNameFirst": "First men",
+  "trustedMenPlaceFirst": "First men place",
+  "trustedMenNameSecond": "Second men",
+  "trustedMenPlaceSecond": "Second men place",
+  "perukirjakoneFee": "90"
+}
+
+export default jsonSchema;
