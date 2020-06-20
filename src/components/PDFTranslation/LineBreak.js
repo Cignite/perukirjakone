cfd52d99495cf7506cbc5b27e070b968fed7f651
Thaman <thaman.chand@invian.fi>
2020-06-20 12:46:58 +0545

    <LineBreak> component

---
+++ src/components/PDFTranslation/LineBreak.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactPDF, {
+  StyleSheet,
+  View
+} from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+  row: {
+    margin: 5,
+    padding: 5,
+    display: "flex",
+    flexDirection: "row"
+  },
+});
+
+const LineBreak = () => (
+  <>
+    <View style={styles.row} />
+  </>
+);
+
+export default LineBreak;
