3ad4c74c1ec8b3f0b84d59f19ca72406e8da0d86
Thaman <thaman.chand@invian.fi>
2020-06-20 12:47:12 +0545

    <List> component

---
+++ src/components/PDFTranslation/List.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Text, View, StyleSheet } from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    marginBottom: 5,
+    marginLeft: 30,
+    marginTop: -5,
+    marginBottom: 15
+  },
+  bulletPoint: {
+    width: 10,
+    fontSize: 15,
+  },
+  itemContent: {
+    flex: 1,
+    fontSize: 15,
+    fontFamily: 'Lato',
+  },
+});
+
+const List = ({ children }) => children;
+
+export const Item = ({ children }) => (
+  <View style={styles.item}>
+    <Text style={styles.bulletPoint}>•</Text>
+    <Text style={styles.itemContent}>{children}</Text>
+  </View>
+);
+
+export default List;
