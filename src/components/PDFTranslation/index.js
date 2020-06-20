f4ee7f7fee6cb44e143000416ecdc690dbbeb2dc
Thaman <thaman.chand@invian.fi>
2020-06-20 12:41:20 +0545

    PDFTranslation component, register Fonts, render Page component in a landscape mode

---
+++ src/components/PDFTranslation/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import moment from 'moment';
+import ReactPDF, {
+  Text,
+  Document,
+  Font,
+  StyleSheet,
+  Image,
+  View,
+} from '@react-pdf/renderer';
+
+import Page from './Page';
+
+import OpenSans from './fonts/Open_Sans/OpenSans-Regular.ttf';
+import LatoRegular from './fonts/Lato/Lato-Regular.ttf';
+import LatoBold from './fonts/Lato/Lato-Bold.ttf';
+
+Font.register( {
+  family: 'Open Sans',
+  src: OpenSans,
+});
+Font.register( {
+  family: 'Lato',
+  src: LatoRegular,
+});
+Font.register( {
+  family: 'Lato Bold',
+  src: LatoBold,
+});
+
+const MyDocument = () => (
+  <Document>
+    <Page orientation="landscape" size="A4" />
+  </Document>
+  );
+
+
+export default MyDocument;
