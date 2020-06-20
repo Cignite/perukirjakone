d2084c8d7107d19146a2a98130a3305ab467206c
Thaman <thaman.chand@invian.fi>
2020-06-20 15:48:14 +0545

    render PDFDocument in Step4

--- src/components/WizardForm/components/Step4/index.js
+++ src/components/WizardForm/components/Step4/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import { PDFViewer } from '@react-pdf/renderer';
+import { Link } from 'react-router-dom';
+
+import PDFDocument from '../../../PDFTranslation';
+
 const Step4 = () => {
   return (
-    <p>Review </p>
+    <>
+      <PDFViewer height="600px" width="100%" >
+        <PDFDocument />
+      </PDFViewer>
+      <br />
+      <br />
+    </>
   )
 }
 
