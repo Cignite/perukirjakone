8877f2fa2185b769057ae40f7b2b32a5345f503c
Thaman <thaman.chand@invian.fi>
2020-06-20 16:23:18 +0545

    Download pdf and style download button and text

--- src/components/WizardForm/components/Step5/index.js
+++ src/components/WizardForm/components/Step5/index.js
@@ -1,30 +1,29 @@
 import React from 'react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+
+import PDFDocument from '../../../PDFTranslation';
+
+import './styles.scss';
 
 const Step4 = () => {
   return (
-    <>
-      <p>
-        This Agreement shall be binding upon and shall inure to the benefit of the parties hereto and their respective successors and assigns; provided, however, that neither this Agreement nor any of the rights, interests or obligations hereunder shall be assigned by any party hereto without the prior written consent of the other party.  Except for the provisions of Article III this Section 12.5 and as otherwise specifically provided in Section 7.7, which is intended to be for the benefit of, and shall be enforceable by, each Indemnified Party, and his or her heirs and representatives, this Agreement, including the documents and instruments referred to in this Agreement, is not intended and does not to confer upon any Person, other than the parties to this Agreement, any rights or remedies under this Agreement.
-      </p>
-      <br />
-
-      <p> 12.6. Complete Agreement. This Agreement, including the Exhibits and Disclosure Schedules hereto and the documents and other writings referred to herein or therein or delivered pursuant hereto, and the Confidentiality Agreement, contains the entire agreement and understanding of the parties with respect to its subject matter. There are no restrictions, agreements, promises, warranties, covenants or undertakings between the parties other than those expressly set forth herein or therein. This Agreement supersedes all prior agreements and understandings (other than the Confidentiality Agreement) between the parties, both written and oral, with respect to its subject matter.
-      </p>
-      <br />
-      <p>
-       12.7. Counterparts. This Agreement may be executed in one or more counterparts all of which shall be considered one and the same agreement and each of which shall be deemed an original.  A facsimile copy or electronic transmission of a signature page shall be deemed to be an original signature page.
-     </p>
-     <br />
-     <p>
-       12.8. Severability. In the event that any one or more provisions of this Agreement shall for any reason be held invalid, illegal or unenforceable in any respect, by any court of competent jurisdiction, such invalidity, illegality or unenforceability shall not affect any other provisions of this Agreement and the parties shall use their reasonable efforts to substitute a valid, legal and enforceable provision which, insofar as practical, implements the purposes and intents of this Agreement. 12.9. Governing Law. This Agreement shall be governed by the laws of the State of Louisiana, without giving effect to its principles of conflicts of laws.
-    </p>
-    <br />
-    <p>
-       12.10. Interpretation. When a reference is made in this Agreement to Sections or Exhibits, such reference shall be to a Section of or Exhibit to this Agreement unless otherwise indicated.  The recitals hereto constitute an integral part of this Agreement.  References to Sections include subsections, which are part of the related Section (e.g., a section numbered "Section 5.5.1" would be part of "Section 5.5" and references to "Section 5.5" would also refer to material contained in the subsection described as "Section 5.5.1").  The table of contents, index and headings contained in this Agreement are for reference purposes only and shall not affect in any way the meaning or interpretation of this Agreement.  Whenever the words "include," "includes" or "including" are used in this Agreement, they shall be deemed to be followed by the words "without limitation".  The phrases "the date of this Agreement", "the date hereof" and terms of similar import, unless the context otherwise requires, shall be deemed to refer to the date set forth in the Recitals to this Agreement.  The parties have participated jointly in the negotiation and drafting of this Agreement.  In the event an ambiguity or question of intent or interpretation arises, this Agreement shall be construed as if drafted jointly by the parties and no presumption or burden of proof shall arise favoring or disfavoring any party by virtue of the authorship of any of the provisions of this Agreement. 12.11. Specific Performance. The parties hereto agree that irreparable damage would occur in the event that the provisions contained in this Agreement were not performed in accordance with their specific terms or were otherwise breached.  It is accordingly agreed that the parties shall be entitled to seek an injunction or injunctions to prevent breaches of this Agreement and to seek to enforce specifically the terms and provisions thereof in the State of Louisiana, this being in addition to any other remedy to which they are entitled at law or in equity.  Each party agrees that it will not seek and will agree to waive any requirement for the securing or posting of a bond in connection with the other party's seeking or obtaining such injunctive relief.  In addition, each of the parties hereto (a) consents to submit itself to the personal jurisdiction of any federal or state court located in the State of Louisiana in the event any dispute arises out of this Agreement or the transactions contemplated by this Agreement, (b) agrees that it will not attempt to deny or defeat such personal jurisdiction by motion or other request for leave from any such court, and (c) agrees that it will not bring any action relating to this Agreement or the transactions contemplated by this Agreement in any court other than a federal or state court located in the State of Louisiana.
-    </p>
-    <br />
-    <br/>
-    </>
+    <div className="downloadStep">
+      <div className="columns is-centered">
+        <p className="thankYou">Thank you for using Perukirjakone service</p>
+        <br />
+      </div>
+      <div className="columns is-centered">
+        <PDFDownloadLink
+          document={<PDFDocument />}
+          fileName="document.pdf"
+          className="button is-primary downloadBtn"
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? "Loading document..." : "Download Pdf"
+          }
+        </PDFDownloadLink>
+      </div>
+    </div>
   )
 }
 
