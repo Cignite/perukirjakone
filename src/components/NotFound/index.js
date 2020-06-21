80160d7db5e4eaaf99099b21a6df422bb7f05e91
Thaman <thaman.chand@invian.fi>
2020-06-21 17:45:02 +0545

    404 page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -5,13 +5,27 @@
  */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <article>
-      <h1>
-        Oh! page you're can't be found!
-      </h1>
-    </article>
+    <section class="hero is-primary is-large">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            404
+          </h1>
+          <h2 class="subtitle">
+              Page not found :(<br />
+              The requested page could not be found.
+          </h2>
+          <br />
+          <h2 class="subtitle">
+            <Link to="/">Go to homepage</Link>
+          </h2>
+        </div>
+      </div>
+    </section>
+
   );
 }
