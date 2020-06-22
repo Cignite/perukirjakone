35b220418ac9c8c0cd5834f491886d8860378b91
Thaman <thaman.chand@invian.fi>
2020-06-22 12:17:13 +0545

    change download button color, align text

--- src/components/WizardForm/components/Step5/index.js
+++ src/components/WizardForm/components/Step5/index.js
@@ -8,18 +8,21 @@ import './styles.scss';
 const Step4 = () => {
   return (
     <div className="downloadStep">
-      <div className="columns is-centered">
+      <div className="is-centered">
         <p className="thankYou">Thank you for using Perukirjakone service</p>
+        <br />
+        <p className="thankYou">Click to download file</p>
+        <br />
         <br />
       </div>
       <div className="columns is-centered">
         <PDFDownloadLink
           document={<PDFDocument />}
           fileName="document.pdf"
-          className="button is-primary downloadBtn"
+          className="button is-dark downloadBtn"
         >
           {({ blob, url, loading, error }) =>
-            loading ? "Loading document..." : "Download Pdf"
+            loading ? "Loading document..." : "Download file"
           }
         </PDFDownloadLink>
       </div>
