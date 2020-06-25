47726938922f9b69fda3d1cf639f74b695974fe5
Thaman <thaman.chand@invian.fi>
2020-06-25 17:28:18 +0545

    <IntroInfo > component created

---
+++ src/components/PDFTranslation/IntroInfo.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  View
+} from '@react-pdf/renderer';
+import moment from 'moment';
+
+const styles = StyleSheet.create({
+  row: {
+    margin: 10,
+    padding: 10,
+    display: "flex",
+    flexDirection: "row"
+  },
+  col: {
+    margin: 5,
+    padding: 5,
+  },
+  border: {
+    borderStyle: "solid",
+    borderBottomColor: "#2d2d2d",
+    borderBottomWidth: 1
+  },
+  borderBox: {
+    borderStyle: "dashed",
+    borderColor: "#2d2d2d",
+    borderWidth: 1
+  },
+  header: {
+    paddingLeft: 30,
+    marginBottom: -30,
+    fontSize: '20px',
+    fontFamily: 'Lato Bold',
+    color: 'black'
+  },
+  colHeader: {
+    fontSize: 15,
+    fontFamily: 'Lato Bold',
+  },
+  colText: {
+    fontSize: 15,
+    fontFamily: 'Lato',
+    color: '#000000'
+  },
+});
+
+const IntroInfo = ({ jsonSchema }) => (
+  <>
+    <View style={styles.row}>
+      <View style={[styles.col]}>
+        <View>
+          <Text style={styles.colHeader}>Aika ja paikka: </Text>
+        </View>
+      </View>
+      <View style={[styles.col]}>
+        <View>
+          <Text style={styles.colText}>{jsonSchema && moment(jsonSchema.inviteeDate).format('DD.MM.YYYY')} &nbsp;&nbsp;{jsonSchema && jsonSchema.inviteePlace}</Text>
+        </View>
+      </View>
+    </View>
+  </>
+);
+
+export default IntroInfo;
