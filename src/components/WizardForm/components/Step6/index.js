7ccc2cde8099a7d10252b3bcb5510db235619b4f
Thaman <thaman.chand@invian.fi>
2020-06-25 11:27:20 +0545

    Add Step6 component as a download component

---
+++ src/components/WizardForm/components/Step6/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+
+import PDFDocument from '../../../PDFTranslation';
+
+import './styles.scss';
+
+const Step4 = () => {
+  return (
+    <div className="downloadStep">
+      <div className="is-centered">
+        <p className="thankYou">Thank you for using Perukirjakone service</p>
+        <br />
+        <p className="thankYou">Click to download file</p>
+        <br />
+        <br />
+      </div>
+      <div className="columns is-centered">
+        <PDFDownloadLink
+          document={<PDFDocument />}
+          fileName="document.pdf"
+          className="button is-dark downloadBtn"
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? "Loading document..." : "Download file"
+          }
+        </PDFDownloadLink>
+      </div>
+    </div>
+  )
+}
+
+export default Step4;
