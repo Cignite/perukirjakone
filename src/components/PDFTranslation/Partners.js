edba400bf8fb2c3358e56e871aaee0e6847a108f
thaman <thaman.me@gmail.com>
2021-02-28 10:33:26 +0200

    Finalise Partners and PresentAtMeeting pages layout

--- src/components/PDFTranslation/Partners.js
+++ src/components/PDFTranslation/Partners.js
@@ -14,166 +14,61 @@ const COLN_WIDTH = (100 - COL1_WIDTH) / 3;
 
 const styles = StyleSheet.create({
   row: {
-    margin: 10,
-    padding: 10,
+    padding: 5,
     display: "flex",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   col: {
-    margin: 10,
-    padding: 10,
+    padding: 5,
   },
-  border: {
-    borderStyle: "solid",
-    borderBottomColor: "#2d2d2d",
-    borderBottomWidth: 1
-  },
-  borderBox: {
-    borderStyle: "dashed",
-    borderColor: "#2d2d2d",
-    borderWidth: 1
-  },
-  header: {
-    paddingLeft: 30,
-    marginBottom: -30,
-    fontSize: '20px',
-    fontFamily: 'Lato Bold',
-    color: 'black'
+  colText: {
+    fontSize: 15,
+    fontFamily: 'Lato',
+    color: '#000000',
   },
   colHeader: {
     fontSize: 15,
     fontFamily: 'Lato Bold',
   },
-  colText: {
-    fontSize: 15,
-    fontFamily: 'Lato',
-    color: '#A8026F',
+  marginLeft: {
+    marginLeft: 80,
   },
-  detailContainer: {
-    display: "flex",
-    flexDirection: 'row',
-  },
-  item: {
-    flexDirection: 'row',
-    marginBottom: 1,
-  },
-  bulletPoint: {
-    width: 10,
-    fontSize: 10,
-  },
-  itemContent: {
-    flex: 10,
-    fontSize: 10,
-    fontFamily: 'Lato',
-    flexGrow: 1
-  },
-  detailText: {
-    fontSize: 15,
-    marginLeft: 30,
-    marginTop: 10,
-    fontFamily: 'Lato Bold',
-    marginBottom: 15
-  },
-  table: {
-    marginLeft: 30,
-    display: "table",
-    width: "auto",
-    borderStyle: BORDER_STYLE,
-    borderColor: BORDER_COLOR,
-    borderWidth: 1,
-    borderRightWidth: 0,
-    borderBottomWidth: 0
-  },
-  tableRow: {
-    margin: "auto",
-    flexDirection: "row"
-  },
-  tableCol1Header: {
-    width: COL1_WIDTH + '%',
-    borderStyle: BORDER_STYLE,
-    borderColor: BORDER_COLOR,
-    borderBottomColor: '#000',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableColHeader: {
-    width: COLN_WIDTH + "%",
-    borderStyle: BORDER_STYLE,
-    borderColor: BORDER_COLOR,
-    borderBottomColor: '#000',
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol1: {
-    width: COL1_WIDTH + '%',
-    borderStyle: BORDER_STYLE,
-    borderColor: BORDER_COLOR,
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCol: {
-    width: COLN_WIDTH + "%",
-    borderStyle: BORDER_STYLE,
-    borderColor: BORDER_COLOR,
-    borderWidth: 1,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  tableCellHeader: {
-    margin: 5,
-    fontSize: 12,
-    fontWeight: 500
-  },
-  tableCell: {
-    margin: 5,
-    fontSize: 10
+  col1: {
+    padding: 0
   }
 });
 
 const Partners = ({ jsonSchema }) => (
   <>
-    <View style={styles.detailContainer}>
-      <Text style={styles.detailText}>Oikeudenomistajat</Text>
-    </View>
-    <View style={styles.table}>
-      <View style={styles.tableRow}>
-        <View style={styles.tableCol1Header}>
-          <Text style={styles.tableCellHeader}>sukulaisuussuhde</Text>
-        </View>
-        <View style={styles.tableCol1Header}>
-          <Text style={styles.tableCellHeader}>Henkilo</Text>
-        </View>
-        <View style={styles.tableCol1Header}>
-          <Text style={styles.tableCellHeader}>SSN</Text>
-        </View>
-        <View style={styles.tableCol1Header}>
-          <Text style={styles.tableCellHeader}>Paikka</Text>
+    <View style={styles.row}>
+      <View style={[styles.col]}>
+        <View>
+          <Text style={styles.colHeader}>Oikeudenomistajat </Text>
         </View>
       </View>
-      {jsonSchema && jsonSchema.relationshipInfo && jsonSchema.relationshipInfo.map((item, index) => {
-        return (
-          <View style={styles.tableRow} key={index}>
-            <View style={styles.tableCol1}>
-              <Text style={styles.tableCell}>{item.relationType}</Text>
-            </View>
-
-            <View style={styles.tableCol1}>
-              <Text style={styles.tableCell}>{item.name}</Text>
-            </View>
-            <View style={styles.tableCol1}>
-              <Text style={styles.tableCell}>{item.ssn}</Text>
-            </View>
-            <View style={styles.tableCol1}>
-              <Text style={styles.tableCell}>{item.address}</Text>
-            </View>
-          </View>
-        )
-      })}
     </View>
-    <LineBreak />
+    <View style={styles.row}>
+      <View style={styles.marginLeft}>
+        {jsonSchema && jsonSchema.relationshipInfo.map((item, index) => {
+          return (
+            <View key={index} style={styles.marginLeft}>
+              <Text style={styles.colText}>
+                {index + 1}.&nbsp;{item.relationType},&nbsp;
+                {item.name},&nbsp;
+                {item.ssn},&nbsp;
+
+              </Text>
+              <View style={styles.row}>
+                <View style={[styles.col1]}>
+                  <Text style={styles.colText}>{item.address}</Text>
+                </View>
+              </View>
+
+            </View>
+            )
+          })}
+        </View>
+      </View>
   </>
 );
 
