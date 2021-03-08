4d944cd59281177acb211ab0eb0ad6c46be072a9
thaman <thaman.me@gmail.com>
2021-03-08 21:20:14 +0200

    formatNumber util

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,21 +1,27 @@
 // create unique booking ID
 export const uniqueDocumentCodeId = () => {
-  const numberOfChars = 6
+  const numberOfChars = 6;
   const letters = "A1B2C3D4E5F6GH7J8K9MNPQRSTUXY";
   let result = "";
   while (result.length < numberOfChars) {
-    const randInt = Math.floor((Math.random() * 19) + 1);
-    const randChr= letters[randInt];
-    if (result.substr(-1, 1)!== randChr) result += randChr;
+    const randInt = Math.floor(Math.random() * 19 + 1);
+    const randChr = letters[randInt];
+    if (result.substr(-1, 1) !== randChr) result += randChr;
   }
-  return (result);
-}
+  return result;
+};
 
 // get sum of value prop across all objects in array
 export const calculateTotal = (arrayObj) => {
-  const getTotal = arrayObj && arrayObj.reduce((accumulator, currentVal) => {
-    return accumulator + parseInt(currentVal.value, 10);
-  }, 0);
+  const getTotal =
+    arrayObj &&
+    arrayObj.reduce((accumulator, currentVal) => {
+      return accumulator + parseInt(currentVal.value, 10);
+    }, 0);
 
   return getTotal;
-}
+};
+
+export const formatNumber = (num) => {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+};
