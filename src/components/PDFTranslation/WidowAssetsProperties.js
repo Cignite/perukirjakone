a0d221276318220ceb70f7abcd41f913f3a36682
thaman <thaman.me@gmail.com>
2021-03-08 21:21:10 +0200

    created WidowAssetsProperties and WidowBlanceSummary components

---
+++ src/components/PDFTranslation/WidowAssetsProperties.js
@@ -0,0 +1,259 @@
+import React from "react";
+import { Text, StyleSheet, View } from "@react-pdf/renderer";
+import moment from "moment";
+
+import { calculateTotal, formatNumber } from "../utils";
+
+const COL1_WIDTH = 200;
+const COLN_WIDTH = (100 - COL1_WIDTH) / 3;
+
+const styles = StyleSheet.create({
+  row: {
+    padding: 5,
+    display: "flex",
+    flexDirection: "row",
+  },
+  col: {
+    padding: 5,
+  },
+  colText: {
+    fontSize: 12,
+    fontFamily: "Lato",
+    color: "#000000",
+  },
+  colHeader: {
+    fontSize: 15,
+    fontFamily: "Lato Bold",
+  },
+  marginLeft: {
+    marginLeft: 65,
+  },
+  col1: {
+    padding: 0,
+  },
+  table: {
+    marginLeft: 30,
+    display: "table",
+    width: "auto",
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+    marginLeft: 40,
+    fontSize: 15,
+    fontFamily: "Lato",
+    color: "#000000",
+  },
+
+  tableCol1: {
+    width: COL1_WIDTH + "%",
+  },
+  tableCol: {
+    width: COLN_WIDTH + "%",
+  },
+  tableCell: {
+    fontSize: 12,
+    fontFamily: "Lato",
+    color: "#000000",
+  },
+});
+
+const WidowAssetsProperties = ({ jsonSchema }) => {
+  const widowBankInfoTotal = calculateTotal(
+    jsonSchema && jsonSchema.widowBankInfo
+  );
+  const widowStockInfo = calculateTotal(
+    jsonSchema && jsonSchema.widowStockInfo
+  );
+  const widowProperty = calculateTotal(jsonSchema && jsonSchema.widowProperty);
+  const widowMotorBikeBrandTypeInfo = calculateTotal(
+    jsonSchema && jsonSchema.widowMotorBikeBrandTypeInfo
+  );
+  const personalWorthInfo = calculateTotal(
+    jsonSchema && jsonSchema.personalWorthInfo
+  );
+  const widowPersonalBelonings = calculateTotal(
+    jsonSchema && jsonSchema.widowPersonalBelonings
+  );
+
+  return (
+    jsonSchema &&
+    jsonSchema.wasDeceasedPersonMarried && (
+      <>
+        <View style={styles.row}>
+          <View style={[styles.col]}>
+            <View>
+              <Text style={styles.colHeader}>IV. Lesken varat </Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.row}>
+          <View style={styles.marginLeft}>
+            <Text style={styles.colText}>
+              Rahavarat ja saatavat korkoineen per{" "}
+              {moment(jsonSchema && jsonSchema.customerTimeOfDeath).format(
+                "DD.MM.YYYY"
+              )}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.table}>
+          {jsonSchema &&
+            jsonSchema &&
+            jsonSchema.widowBankInfo &&
+            jsonSchema.widowBankInfo.map((item, index) => {
+              return (
+                <View style={styles.tableRow} key={index}>
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.name}</Text>
+                  </View>
+
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.value}</Text>
+                  </View>
+                </View>
+              );
+            })}
+        </View>
+
+        <View style={styles.table}>
+          {jsonSchema &&
+            jsonSchema &&
+            jsonSchema.widowStockInfo &&
+            jsonSchema.widowStockInfo.map((item, index) => {
+              return (
+                <View style={styles.tableRow} key={index}>
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.name}</Text>
+                  </View>
+
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.value}</Text>
+                  </View>
+                </View>
+              );
+            })}
+        </View>
+
+        <View style={styles.table}>
+          {jsonSchema &&
+            jsonSchema.widowProperty &&
+            jsonSchema.widowProperty.map((item, index) => {
+              return (
+                <View style={styles.tableRow} key={index}>
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.name}</Text>
+                  </View>
+
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.value}</Text>
+                  </View>
+                </View>
+              );
+            })}
+        </View>
+
+        <View style={styles.table}>
+          {jsonSchema &&
+            jsonSchema.propertyInfo &&
+            jsonSchema.propertyInfo.map((item, index) => {
+              return (
+                <View style={styles.tableRow} key={index}>
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.name}</Text>
+                  </View>
+
+                  <View style={styles.tableCol1}>
+                    <Text style={styles.tableCell}>{item.value}</Text>
+                  </View>
+                </View>
+              );
+            })}
+        </View>
+
+        {jsonSchema && jsonSchema && jsonSchema.didWidowHadCarBoat && (
+          <View style={styles.table}>
+            {jsonSchema &&
+              jsonSchema.widowMotorBikeBrandTypeInfo &&
+              jsonSchema.widowMotorBikeBrandTypeInfo.map((item, index) => {
+                return (
+                  <View style={styles.tableRow} key={index}>
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.name}</Text>
+                    </View>
+
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.value}</Text>
+                    </View>
+                  </View>
+                );
+              })}
+          </View>
+        )}
+
+        {jsonSchema && jsonSchema && jsonSchema.isPersonalWorth && (
+          <View style={styles.table}>
+            {jsonSchema &&
+              jsonSchema.personalWorthInfo &&
+              jsonSchema.personalWorthInfo.map((item, index) => {
+                return (
+                  <View style={styles.tableRow} key={index}>
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.name}</Text>
+                    </View>
+
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.value}</Text>
+                    </View>
+                  </View>
+                );
+              })}
+          </View>
+        )}
+
+        {jsonSchema && jsonSchema && jsonSchema.isPersonalBelonings && (
+          <View style={styles.table}>
+            {jsonSchema &&
+              jsonSchema.widowPersonalBelonings &&
+              jsonSchema.widowPersonalBelonings.map((item, index) => {
+                return (
+                  <View style={styles.tableRow} key={index}>
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.name}</Text>
+                    </View>
+
+                    <View style={styles.tableCol1}>
+                      <Text style={styles.tableCell}>{item.value}</Text>
+                    </View>
+                  </View>
+                );
+              })}
+          </View>
+        )}
+
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            <View style={styles.tableCol1}>
+              <Text style={styles.tableCell}>Yhteensä</Text>
+            </View>
+
+            <View style={styles.tableCol1}>
+              <Text style={styles.tableCell}>
+                {widowBankInfoTotal +
+                  widowStockInfo +
+                  widowProperty +
+                  widowMotorBikeBrandTypeInfo +
+                  personalWorthInfo +
+                  widowPersonalBelonings}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </>
+    )
+  );
+};
+
+export default WidowAssetsProperties;
---
+++ src/components/PDFTranslation/WidowBalanceSummary.js
@@ -0,0 +1,144 @@
+import React from "react";
+import { Text, StyleSheet, View } from "@react-pdf/renderer";
+
+import { calculateTotal, formatNumber } from "../utils";
+
+const BORDER_COLOR = "#000";
+const BORDER_STYLE = "solid";
+const COL1_WIDTH = 200;
+const COLN_WIDTH = (100 - COL1_WIDTH) / 3;
+
+const styles = StyleSheet.create({
+  row: {
+    padding: 5,
+    display: "flex",
+    flexDirection: "row",
+  },
+  col: {
+    padding: 5,
+  },
+  colText: {
+    fontSize: 12,
+    fontFamily: "Lato",
+    color: "#000000",
+  },
+  colHeader: {
+    fontSize: 15,
+    fontFamily: "Lato Bold",
+  },
+  marginLeft: {
+    marginLeft: 65,
+  },
+  col1: {
+    padding: 0,
+  },
+  table: {
+    marginLeft: 30,
+    display: "table",
+    width: "auto",
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+    marginLeft: 40,
+    fontSize: 15,
+    fontFamily: "Lato",
+    color: "#000000",
+  },
+
+  tableCol1: {
+    width: COL1_WIDTH + "%",
+  },
+  tableCol: {
+    width: COLN_WIDTH + "%",
+  },
+  tableCell: {
+    fontSize: 12,
+    fontFamily: "Lato",
+    color: "#000000",
+    fontWeight: "bold",
+  },
+});
+
+const WidowBalanceSummary = ({ jsonSchema }) => {
+  const widowBankInfoTotal = calculateTotal(
+    jsonSchema && jsonSchema.widowBankInfo
+  );
+  const widowStockInfo = calculateTotal(
+    jsonSchema && jsonSchema.widowStockInfo
+  );
+  const widowProperty = calculateTotal(jsonSchema && jsonSchema.widowProperty);
+  const widowMotorBikeBrandTypeInfo = calculateTotal(
+    jsonSchema && jsonSchema.widowMotorBikeBrandTypeInfo
+  );
+  const personalWorthInfo = calculateTotal(
+    jsonSchema && jsonSchema.personalWorthInfo
+  );
+  const widowPersonalBelonings = calculateTotal(
+    jsonSchema && jsonSchema.widowPersonalBelonings
+  );
+
+  return (
+    <>
+      <View style={styles.row}>
+        <View style={[styles.col]}>
+          <View>
+            <Text style={styles.colHeader}>V. Yhteenveto </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>Lesken varat</Text>
+          </View>
+
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>
+              {widowBankInfoTotal +
+                widowStockInfo +
+                widowProperty +
+                widowMotorBikeBrandTypeInfo +
+                personalWorthInfo +
+                widowPersonalBelonings}
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>Lesken velat</Text>
+          </View>
+
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>-</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.table}>
+        <View style={styles.tableRow}>
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>Lesken omaisuuden säästö</Text>
+          </View>
+
+          <View style={styles.tableCol1}>
+            <Text style={styles.tableCell}>
+              {widowBankInfoTotal +
+                widowStockInfo +
+                widowProperty +
+                widowMotorBikeBrandTypeInfo +
+                personalWorthInfo +
+                widowPersonalBelonings}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default WidowBalanceSummary;
