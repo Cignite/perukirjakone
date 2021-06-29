2088d485d53685b635634f685a4322c23f5a249b
Thaman Chand <thaman.chand@invian.fi>
2021-06-29 20:56:48 +0300

    remove dummy testimonials with real ones

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -9,17 +9,9 @@ function Testimonials(props) {
         <div className="Testimonials__column column" key={index}>
           <div className="Testimonials__card card">
             <div className="Testimonials__card-content card-content has-text-centered">
-              <div className="Testimonials__avatar">
-                <div className="Testimonials__image image">
-                  <img src={item.avatarImage} alt="Perukirjakone" />
-                </div>
-              </div>
               <p className="Testimonials__quote">"{item.bio}"</p>
               <div className="Testimonials__info">
                 <div className="has-text-weight-bold">{item.name}</div>
-                <a className="link" href={item.companyUrl}>
-                  {item.company}
-                </a>
               </div>
             </div>
           </div>
--- src/components/TestimonialsSection/index.js
+++ src/components/TestimonialsSection/index.js
@@ -12,33 +12,19 @@ function TestimonialsSection(props) {
         <Testimonials
           items={[
             {
-              headerImage:
-                "https://images.unsplash.com/photo-1492138623748-a1b1f758a762",
-              avatarImage: "http://i.pravatar.cc/150?img=68",
+              name: "Tuomas N",
               bio:
-                "Serveur heberger sur {Site}, utilisant Fivem et disposant de XXX slots joueur. tout les mode sont issue du processus de developpement officiel Fivem pour garantir une experienc ede jeu maximale.",
-              company: "company.com",
-              companyUrl: "https://company.com",
+                "Palvelulla sai tehtyä perukirjan. Pidin siitä, että prosessia pystyi tekemään vaiheittain. Lopullisen version tekemiseen meni pari viikkoa",
             },
             {
-              headerImage: "https://source.unsplash.com/uwPBAlQyIag/1080x360",
-              avatarImage: "http://i.pravatar.cc/150?img=35",
-              name: "Lisa Zinn",
-              role: "Software Engineer",
+              name: "Elise V",
               bio:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis totam!",
-              company: "company.com",
-              companyUrl: "https://company.com",
+                "Tein perukirjan palvelulla. Oli melko helppo prosessi, asiat oli selkeästi selitetty. Kiitos paljon",
             },
             {
-              headerImage: "https://source.unsplash.com/pJ_DCj9KswI/1080x360",
-              avatarImage: "http://i.pravatar.cc/150?img=16",
-              name: "Diana Low",
-              role: "Designer",
+              name: "Petteri Vuorio",
               bio:
-                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae.",
-              company: "company.com",
-              companyUrl: "https://company.com",
+                "Kaikki sujui hienosti. Epäselvätkin asiat tulivat selvitettyä.",
             },
           ]}
         />
