cc72414ffdf7d3f46459ab4a5784c267b0b10c74
Thaman Chand <thaman@aiforsite.com>
2022-01-08 13:35:19 +0200

    added RenderMaskInput component

---
+++ src/components/Shared/MaskInput/Index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import MaskedInput from 'react-text-mask';
+import PropTypes from 'prop-types';
+
+const RenderMaskInput = ({
+  input, meta, disabled, placeholder, type, label, mask, className, guide = true, pipe, keepCharPositions = false,
+}) => (
+  
+
+  <div className="field">
+      <label className="label">{label}</label>
+      <div className="control">
+      <MaskedInput
+        {...input}
+        placeholder={placeholder}
+        type={type}
+        mask={[/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /[A-Za-z]/]}
+        className={className}
+        guide={guide}
+        pipe={pipe}
+        keepCharPositions={keepCharPositions}
+        disabled={disabled}
+        className={meta.touched && meta.error ? "input is-danger" : "input"}
+
+        />
+      </div>
+    </div>
+
+);
+
+RenderMaskInput.propTypes = {
+  input: PropTypes.shape().isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  mask: PropTypes.arrayOf(PropTypes.any).isRequired,
+  className: PropTypes.string.isRequired,
+  guide: PropTypes.bool.isRequired,
+  keepCharPositions: PropTypes.bool.isRequired,
+  pipe: PropTypes.func.isRequired,
+};
+
+RenderMaskInput.defaultProps = {
+  placeholder: '',
+  type: 'text',
+};
+
+export default RenderMaskInput;
